505ff15f3da383ce3d64021bd5d6f074c5b2e28a
Pratik Manure <162863265+Pratikmanure@users.noreply.github.com>
2026-08-16 08:17:35 +0530

    Add 3-sum algorithm in 3_sum_Structure.py
    Implement 3-sum algorithm using two-pointer technique.

---
+++ Pattern01_Two_Pointer /3_sum_Structure.py
@@ -0,0 +1,20 @@
+nums = [-1, 0, 1, 2, -1, -4]
+
+nums.sort()
+print(nums)
+
+result = []
+
+for i in range(len(nums)-2):
+    
+    left = i + 1
+    right = len(nums) - 1
+
+    while left < right:
+        total = nums[i] + nums[left] + nums[right]
+        if total < 0:
+            left += 1
+        elif total > 0:
+            right -= 1
+        else:
+            total = result
