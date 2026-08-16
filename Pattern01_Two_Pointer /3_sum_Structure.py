f5d539607268845da6be4cde0c48914acdad0839
Pratik Manure <162863265+Pratikmanure@users.noreply.github.com>
2026-08-16 20:30:11 +0530

    Implement two-pointer approach for 3-sum problem
    Refactor the 3-sum algorithm to use two pointers and handle duplicates.

--- Pattern01_Two_Pointer /3_sum_Structure.py
+++ Pattern01_Two_Pointer /3_sum_Structure.py
@@ -1,20 +1,27 @@
-nums = [-1, 0, 1, 2, -1, -4]
+#use the concept of two pointers and find the valid triplets which equals to zero.
 
+nums = [-1, 0, 1, 2, -1, -4]
 nums.sort()
 print(nums)
 
 result = []
 
-for i in range(len(nums)-2):
-    
+for i in range(len(nums) - 2):
+    if i > 0 and nums[i] == nums[i - 1]:
+        continue
+
     left = i + 1
     right = len(nums) - 1
 
     while left < right:
         total = nums[i] + nums[left] + nums[right]
+
         if total < 0:
             left += 1
         elif total > 0:
             right -= 1
         else:
-            total = result
+            result.append([nums[i], nums[left], nums[right]])
+            left += 1
+            right -= 1
+print(result)
