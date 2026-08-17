e6242d615fd4a1c677f47c925a23937532893dd5
Pratik Manure <162863265+Pratikmanure@users.noreply.github.com>
2026-08-17 19:43:21 +0530

    Add 4-sum algorithm to find quadruplets
    Implement 4-sum algorithm using two pointers technique.

---
+++ Pattern01_Two_Pointer /4_Sum_Structure.py
@@ -0,0 +1,44 @@
+
+
+nums = [1, 0, -1, 0, -2, 2]
+target = 0
+
+nums.sort()
+
+result = []
+
+for i in range(len(nums) - 3):
+
+    if i > 0 and nums[i] == nums[i - 1]:
+        continue
+
+    for j in range(i + 1, len(nums) - 2):
+
+        if j > i + 1 and nums[j] == nums[j - 1]:
+            continue
+
+        left = j + 1
+        right = len(nums) - 1
+
+        while left < right:
+
+            total = nums[i] + nums[j] + nums[left] + nums[right]
+
+            if total < target:
+                left += 1
+
+            elif total > target:
+                right -= 1
+
+            else:
+                result.append([
+                    nums[i],
+                    nums[j],
+                    nums[left],
+                    nums[right]
+                ])
+
+                left += 1
+                right -= 1
+
+print(result)
